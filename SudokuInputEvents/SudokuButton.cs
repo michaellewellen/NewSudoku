26df1a558b4e0f46ff285a012e01d8c1c5ce44d1
Michael Lewellen <michael.lewellen@snow.edu>
2018-09-18 11:01:09 -0600

    Got basic viewmodel set up

---
+++ SudokuInputEvents/SudokuButton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SudokuInputEvents
+{
+    public class SudokuButton : INotifyPropertyChanged
+    {
+        public SudokuButton(int? value = null)
+        {
+            Value = value;
+        }
+        private int? buttonValue;
+        public int? Value
+        {
+            get { return buttonValue; }
+            set { SetField(ref buttonValue, value); }
+        }
+
+
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
+    }
+}
